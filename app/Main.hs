ccd4b9cfb3d2fc6f0fe0b0ce4447327cfbddb6cf
Harry Pan <pan.xingzhi@gmail.com>
2017-03-04 16:20:34 -0800

    Another page scraper for older pages.

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,17 @@
 import Data.List (intercalate,isInfixOf)
 import Data.List.Split (chunksOf)
+import Data.String.Utils (strip)
 import Text.HTML.Scalpel
-  (Scraper,TagName(TagString)
+  (AttributeName(AttributeString),Scraper,TagName(TagString)
   ,chroot,hasClass,htmls,scrapeStringLike,scrapeURL,tagSelector,texts
-  ,(@:),(//))
+  ,(@=),(@:),(//))
 import Text.Printf (printf)
 
 urlFormat = "https://travel.state.gov/content/visas/en/law-and-policy/bulletin/%d/visa-bulletin-for-%s-%d.html"
-fiscalYears = [2014,2015,2016,2017]
-months = ["october","november","december","january","february","march","april","may","june","july","august","september"]
---fiscalYears = [2017]
---months = ["march"]
+--fiscalYears = [2014,2015,2016,2017]
+--months = ["october","november","december","january","february","march","april","may","june","july","august","september"]
+fiscalYears = [2016]
+months = ["april"]
 
 data TableContent = TableContent Int String [[String]]
 
@@ -30,9 +31,12 @@ scrapePage (fiscalYear,month) =
     url :: String
     url = printf urlFormat fiscalYear month year
     employmentHtmlTables :: IO (Maybe [String])
-    employmentHtmlTables = scrapeURL url pageScraper
+    employmentHtmlTables = scrapeURL url pageScraper >>= \tables -> case tables of
+      Nothing -> scrapeURL url pageScraper2
+      v -> return v
   in employmentHtmlTables >>= return . extractTables year month
 
+-- used for >= 2016 may
 pageScraper :: Scraper String [String]
 pageScraper = chroot (TagString "div" @: [hasClass "Visa_Contentpage_Category"]) tableScraper
 
@@ -49,11 +53,20 @@ extractTable :: Int -> String -> String -> TableContent
 extractTable year month table = case scrapeStringLike table cellScraper of
   Nothing -> TableContent year month []
   Just content ->
-    let cleanContent = map (filter (\x -> x /= '\n' && x /= '\160')) content
+    let cleanContent = map (filter (\x -> x /= '\n' && x /= '\160') . strip) content
         nRows = 9
         nCols = length content `div` nRows
     in TableContent year month $ chunksOf nCols cleanContent
 
 cellScraper :: Scraper String [String]
 cellScraper = texts (tagSelector "tbody" // tagSelector "tr" // tagSelector "td")
+
+-- used for < 2016 may
+pageScraper2 :: Scraper String [String]
+pageScraper2 = chroot (TagString "div" @: [AttributeString "id" @= "main"]) tableScraper2
+
+tableScraper2 :: Scraper String [String]
+tableScraper2 = htmls selector >>= return . filter (isInfixOf "Employ")
+  where selector = (TagString "div" @: [hasClass "visabulletinemploymenttable"])
+                   // tagSelector "table"
 
